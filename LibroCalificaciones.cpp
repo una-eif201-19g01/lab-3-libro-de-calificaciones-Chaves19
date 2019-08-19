8460c84e90a21db55ab2ed314dcc3635ab890507
Chaves19 <53483583+Chaves19@users.noreply.github.com>
2019-08-19 15:34:55 -0600

    terminado

--- LibroCalificaciones.cpp
+++ LibroCalificaciones.cpp
@@ -5,7 +5,7 @@
  *
  *    Description:  Reporte de calificaciones
  *
- *        Created:  2019-08-16
+ *        Created:  2019-08-19
  *
  *         Author:  Alonso Chaves achaqui@yahoo.com
  *   Organization:  Universidad Nacional de Costa Rica
@@ -18,11 +18,11 @@
 #include "LibroCalificaciones.h"
 
 int LibroCalificaciones::obtenerNotaMinima() {
-    int notaMin = 0;
-    for (int i = 0; i < ESTUDIANTES; i++) {
-        for (int j = 0; j < EXAMENES; j++) {
-            if (notaMin <= calificaciones[i][j]) {
-                notaMin = calificaciones[i][j];
+    int notaMin = 100;
+    for (int columnas = 0; columnas < ESTUDIANTES; columnas++) {
+        for (int filas = 0; filas < EXAMENES; filas++) {
+            if (notaMin >= calificaciones[columnas][filas]) {
+                notaMin = calificaciones[columnas][filas];
             }
         }
     }
@@ -31,47 +31,57 @@ int LibroCalificaciones::obtenerNotaMinima() {
 
 int LibroCalificaciones::obtenerNotaMaxima() {
     int notaMax = 100;
-    for (int i = 0; i < ESTUDIANTES; i++) {
-        for (int j = 0; j < EXAMENES; j++) {
-            if (notaMax >= calificaciones[i][j]) {
-                notaMax = calificaciones[i][j];
+    for (int columnas = 0; columnas < ESTUDIANTES; columnas++) {
+		for(int filas = 0; filas < EXAMENES; filas++){ 
+            if (notaMax <= calificaciones[columnas][filas]) {
+                notaMax = calificaciones[columnas][filas];
             }
         }
     }
     return notaMax;
 }
 
-double LibroCalificaciones::obtenerPromedio(const int, const int, int) {
-
+double LibroCalificaciones::obtenerPromedio(int columnas) {
+	float prom = 0;
+	for(int filas = 0; filas < EXAMENES; filas++){
+		prom = prom + calificaciones[columnas][filas];
+	}
+	return (prom / EXAMENES);
 }
 
 LibroCalificaciones::LibroCalificaciones() {
-    for (int i = 0; i < ESTUDIANTES; i++) {
-        for (int j = 0; j < EXAMENES; j++) {
-            calificaciones[i][j] = 0;
+    for (int columnas = 0; columnas < ESTUDIANTES; columnas++) {
+        for (int filas = 0; filas < EXAMENES; filas++) {
+            calificaciones[columnas][filas] = 0;
         }
     }
 }
 
-LibroCalificaciones::LibroCalificaciones(const std::string &NombreCurso, int[][]) {
-    for (int i = 0; i < ESTUDIANTES; i++) {
-        for (int j = 0; j < EXAMENES; j++) {
-            calificaciones[i][j] = 1;
+LibroCalificaciones::LibroCalificaciones(std::string NombreCurso, int arreglo[][EXAMENES]) {
+    for (int columnas = 0; columnas < ESTUDIANTES; columnas++) {
+        for (int filas = 0; filas < EXAMENES; filas++) {
+            calificaciones[columnas][filas] = arreglo[columnas][filas];
         }
     }
 }
-}
+
 
 std::string LibroCalificaciones::obtenerReporteNotas() {
-
+	std::string reporte = "\nLas siguientes son las notas del curso[Curso Progra I]: \n\t\t\t\tExamen 1\tExamen 2\tExamen 3\tPromedio\n";
+	for (int columnas = 0; columnas < ESTUDIANTES; columnas++) {
+		reporte = reporte + "Estudiante [" + std::to_string(columnas + 1) + "]\t\t\t" + std::to_string(calificaciones[columnas][0]) + " \t\t" + std::to_string(calificaciones[columnas][1]) + "\t\t" + std::to_string(calificaciones[columnas][2]) + "\t\t" + std::to_string(obtenerPromedio(columnas)) + "\n";
+	
+	}
+	return reporte;
 }
 
 std::string LibroCalificaciones::obtenerReporteNotasMaxMin() {
-
+	std::string maxMin = "\nLa nota minima es: [ "+std::to_string(obtenerNotaMinima())+" ]\nLa nota maxima es: [ "+std::to_string(obtenerNotaMaxima())+" ]\n";
+	return maxMin;
 }
 
 std::string LibroCalificaciones::getNombreCurso() {
-    return nombreCurso();
+    return nombreCurso;
 }
 
 void LibroCalificaciones::setNombreCurso(std::string nC) {
